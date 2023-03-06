d2ffabbc627271a43625f873f1653f1ef459f2f4
hazlomcodes <harry.lomas1@outlook.com>
2023-03-06 16:35:01 +0000

    tables displaying albeit with wrong names :(

---
+++ migrations/01-create-donation-table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE Donations (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  quantity VARCHAR(255) NOT NULL
+  );
--- src/controllers/donation.js
+++ src/controllers/donation.js
@@ -1,6 +1,13 @@
 // src/controllers/Donation.js
-const createDonation = (req, res) => {
-    res.sendStatus(201);
-}
-
-module.exports = { createDonation }
+const createDonation = async (req, res) => {
+    const { name, quantity } = req.body
+  
+    try {
+      const { rows: [ donation ] } = await db.query(`INSERT INTO Artists (name, genre) VALUES ('${name}', '${quantity}') RETURNING *`)
+      res.status(201).json(donation)
+    } catch (err) {
+      res.status(500).json(err.message)
+    }
+  }
+  
+  module.exports = { createDonation }
