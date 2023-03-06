9ae631f443244716723ca7464d4c2d77954561e7
hazlomcodes <harry.lomas1@outlook.com>
2023-03-06 16:26:39 +0000

    test suite updated

--- tests/donation-create.js
+++ tests/donation-create.js
@@ -1,17 +1,28 @@
 const { expect } = require('chai');
 const request = require('supertest');
+const db = require('../src/db')
 const app = require('../src/app');
 
 describe('create donation', () => {
   describe('/donations', () => {
     describe('POST', () => {
       it('creates a new donation in the database', async () => {
-        const res = await request(app).post('/donations').send({
+        const { status, body} = await request(app).post('/donations').send({
           name: 'Tomato',
           quantity: '1',
         });
 
-        expect(res.status).to.equal(201);
+        expect(status).to.equal(201);
+        expect(body.name).to.equal('Tomato');
+        expect(body.quantity).to.equal('1');
+
+        const {
+          rows: [donationData],
+        } = await db.query(`SELECT * FROM donations WHERE id = ${body.id}`);
+        expect(donationData.name).to.equal('Tomato');
+        expect(donationData.quantity).to.equal('1');
+        
+      
       });
     });
   });
