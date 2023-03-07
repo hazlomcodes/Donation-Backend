aa5913329cda35f4f333d6a1925112537d98947f
hazlomcodes <harry.lomas1@outlook.com>
2023-03-07 19:38:36 +0000

    API linked

--- migrations/01-create-donation-table.sql
+++ migrations/01-create-donation-table.sql
@@ -2,5 +2,5 @@ CREATE TABLE Donations (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   quantity VARCHAR(255) NOT NULL,
-  expiration DATE(255) NOT NULL
+  expiration DATE NOT NULL
   );
